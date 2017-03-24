82ee07eb6d6ec230840fa53484fffba7472d7fba
Esteban Calvo V <estebanedcv@gmail.com>
2017-03-24 14:56:02 -0600

    Continúo con la memoria y agrego el nuevo módulo para manejar las entradas. De momento ninguno de los dos hace nada

---
+++ manejo_entradas.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:54:58 03/24/2017 
+// Design Name: 
+// Module Name:    manejo_entradas 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: maneja las entradas, convirtiendo lo que entra en un registro para
+// poder ser usado por el resto del programa.
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module manejo_entradas(
+    );
+
+
+endmodule
--- manejo_memoria.v
+++ manejo_memoria.v
@@ -26,10 +26,26 @@ module manejo_memoria(
     );
 
 reg [3:0] RAM [0:255];
-parameter count = 0;
+parameter count = -1, pila = 0;
 
 always @ (posedge clk)
 	begin
-		
-		
+		if (count==0)
+		   begin
+				count = count+1;
+				RAM[pila] = boton;
+				pila = pila+1;
+				memoria = boton;
+			end
+		else
+			begin
+				if (piso==0)
+						
+				else if (piso==1)
+				
+				else if (piso==2)
+				
+				else
+				
+			end
 endmodule
