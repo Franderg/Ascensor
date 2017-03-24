ed630da134e9364ccb7e07424fd40d65c2575842
Esteban Calvo V <estebanedcv@gmail.com>
2017-03-24 12:01:39 -0600

    Se arregló la máquina de estados y se agregaron todas las entradas y salidas que vamos a tener.

--- maquina_estados.v
+++ maquina_estados.v
@@ -3,74 +3,167 @@
 
 
 module maquina_estados(
-	 input b_piso1,//botones para llamar al ascensor
-	 input b_piso2,
-	 input b_piso3,
-	 input b_piso4,
-	 input ir_a_piso1,//botones para ir al piso una vez dentro del ascensor
-	 input ir_a_piso2,
-	 input ir_a_piso3,
-	 input ir_a_piso4,
-    input en,
-	 input clk,
-	 input rst,
-    input baja, //se indica cuando baja
-	 input sube, //se indica cuando sube
-    output reg [1:0] piso // 
+	 input reg [3:0] memoria, //entradas donde se guardan todos los botones de los pisos
+	 //0: nadie ha mandado nada
+	 //1: ir a piso 1
+	 //2: ir a piso 2
+	 //3: ir a piso 3
+	 //4: ir a piso 4
+	 //5: piso 1, botón de subir
+	 //6: piso 2, botón de bajar
+	 //7: piso 2, botón de subir
+	 //8: piso 3, botón de bajar
+	 //9: piso 3, botón de subir
+	 //10: piso 4, botón de bajar
+    input en, //enable
+	 input clk, //clock
+	 input rst, //reset
+    output reg [1:0] piso, //Indica el piso actual, 0, 1, 2, 3
+	 output reg [1:0] accion, //Indica si está subiendo o bajando, 0 no se mueve, 1 sube y 2 baja
+	 output puertas //Indica si las puertas están abiertas o cerradas, 0 cerradas, 1 abiertas
     );
 
-	parameter S0=0, S1=1, S2=2, S3=3; //Estados
+	parameter P1=0, P2=1, P3=2, P4=3; //Estados
 	
-	reg [1:0] e_actual; //estado actual
+	reg [1:0] e_actual; //estado actual (piso)
 	reg [1:0] e_siguiente; // estado siguiente
 	
-		
 	always @ *
 		begin
 			case(e_actual)
-				S0:begin
-					if(sube==1)
-						e_siguiente=S1;//si sube es 1, sube piso
-					else
-						e_siguiente=S0;//Si no recibe instruccion se mantiene
+			
+//************ Estado: Piso 1*****************//
+				P1:begin
+				   case(memoria)
+						0:begin
+								piso <= 0;
+								accion <= 0;
+								puertas = 0;
+								e_siguiente=P1;
+							end
+					   1,5:
+							begin
+								piso <= 0;
+								accion <= 0;
+								puertas = 1;
+								e_siguiente=P1;
+							end
+						2,3,4,6,7,8,9,10:begin
+								piso <= 0; //piso1
+								accion <= 1; //sube
+								puertas = 0; //cerradas
+								e_siguiente=P2;
+							end	
+						default:e_siguiente=P1;
+					endcase
+				end
+
+//************ Estado: Piso 2*****************//
+				P2:begin
+					case(memoria)
+						0:begin
+								piso <= 1; //piso 2
+								accion <= 0; //nada
+								puertas = 0; //cerradas
+								e_siguiente=P2;
+							end
+					   1,5:
+							begin
+								piso <= 1;//piso 2
+								accion <= 2;//baja
+								puertas = 0; //cerradas
+								e_siguiente=P1;
+							end
+						2,6,7:begin
+								piso <= 1; //piso 2
+								accion <= 0; //nada
+								puertas = 1;//abiertas
+								e_siguiente=P2; //se queda en piso 2
+							end	
+						3,4,8,9,10:begin
+								piso <= 1; //piso 2
+								accion <= 1; //sube
+								puertas = 0;//cerradas
+								e_siguiente=P3; //vamos para piso 3
+							end
+						default:e_siguiente=P1;
+					endcase
 					end
-				S1:begin
-					if(sube==1)
-						e_siguiente=S2;//si sube es 1, sube piso
-					else if(baja==1)
-						e_siguiente=S0; //si baja = 1, baja un piso
-					else
-						e_siguiente=S1;//Si no recibe instruccion se mantiene
+					
+//************ Estado: Piso 3*****************//
+				P3:begin
+					case(memoria)
+						0:begin
+								piso <= 2; //piso 3
+								accion <= 0; //nada
+								puertas = 0; //cerradas
+								e_siguiente=P3;
+							end
+					   1,2,5,6,7:
+							begin
+								piso <= 2;//piso 3
+								accion <= 2;//baja
+								puertas = 0; //cerradas
+								e_siguiente=P2;
+							end
+						3,8,9:begin
+								piso <= 2; //piso 3
+								accion <= 0; //nada
+								puertas = 1;//abiertas
+								e_siguiente=P3; //se queda en piso 2
+							end	
+						4,10:begin
+								piso <= 2; //piso 3
+								accion <= 1; //sube
+								puertas = 0;//cerradas
+								e_siguiente=P4; //vamos para piso 4
+							end
+						default:e_siguiente=P1;
+					endcase
 					end
-				S2:begin
-					if(sube==1)
-						e_siguiente=S3;//si sube es 1, sube piso
-					else if(baja==1)
-						e_siguiente=S1; //si baja = 1, baja un piso
-					else
-						e_siguiente=S2;//Si no recibe instruccion se mantiene					
+
+//************ Estado: Piso 4*****************//
+				P4:begin
+					case(memoria)
+						0:begin
+								piso <= 3; //piso 4
+								accion <= 0; //nada
+								puertas = 0; //cerradas
+								e_siguiente=P4;
+							end
+					   1,2,3,5,6,7,8,9:
+							begin
+								piso <= 3;//piso 3
+								accion <= 2;//baja
+								puertas = 0; //cerradas
+								e_siguiente=P3; //bajamos al piso 3
+							end
+						4,10:begin
+								piso <= 3; //piso 3
+								accion <= 0; //nada
+								puertas = 1;//abiertas
+								e_siguiente=P4; //se queda en el piso 4
+							end
+						default:e_siguiente=P1;
+					endcase
 					end
-				S3:begin
-					if(baja==1)
-						e_siguiente=S2; //si baja = 1, baja un piso
-					else
-						e_siguiente=S3;//Si no recibe instruccion se mantiene
-					end
-				default: e_siguiente=S0;//por defecto estÃ¡ en el piso 1
+					
+//************ Estado: Defecto*****************//					
+				default: e_siguiente=P1;//por defecto estÃ¡ en el piso 1
 			endcase
 		end
 	
 	always @ (posedge clk or negedge rst)
 		begin
 			if(!rst)
-				e_actual <= S0;
+			   begin
+			      accion <= 0;
+	            piso <= 0;
+	            puertas = 0;	
+				   e_actual <= S0;
+				end
 			else if(en)
 				e_actual <= e_siguiente;
-		end
-			
-	always @*
-		begin
-			piso=e_actual; //de manera inmediara piso tiene el estado actual
 		end
 
 endmodule
